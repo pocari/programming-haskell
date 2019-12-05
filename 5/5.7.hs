751fc5d41491f5a86d6fa4fb231c109c511f16f3
pocari <caffelattenonsugar@gmail.com>
2019-12-06 00:43:32 +0900

    〜5.7.9

--- 5/5.7.hs
+++ 5/5.7.hs
@@ -12,3 +12,35 @@ grid m n = [ (x, y) | x <- [0 .. m], y <- [0 .. n] ]
 square :: Int -> [(Int, Int)]
 square n = [ (x, y) | (x, y) <- grid n n, x /= y ]
 
+-- 5.7.4
+myReplicate :: Int -> a -> [a]
+myReplicate n x = [ x | _ <- [1 .. n] ]
+
+-- 5.7.5
+pyths :: Int -> [(Int, Int, Int)]
+pyths n =
+  [ (x, y, z)
+  | x <- [1 .. n]
+  , y <- [1 .. n]
+  , z <- [1 .. n]
+  , x * x + y * y == z * z
+  ]
+
+-- 5.7.6
+perfects :: Int -> [Int]
+perfects n = [ x | x <- [1 .. n], isPerfect x ]
+ where
+  isPerfect y = y == yakusuSum y
+  yakusuSum m = sum [ z | z <- [1 .. (m - 1)], m `mod` z == 0 ]
+
+-- 5.7.7
+q5_7_7 :: [(Int, Int)]
+q5_7_7 = concat [ [ (x, y) | y <- [4, 5, 6] ] | x <- [1, 2, 3] ]
+
+-- 5.7.8
+-- 問題にでてくる find 関数がどれのことかよくわからなかった
+
+-- 5.7.9
+scalarproduct :: [Int] -> [Int] -> Int
+scalarproduct xs ys = sum [ x * y | (x, y) <- zip xs ys ]
+
