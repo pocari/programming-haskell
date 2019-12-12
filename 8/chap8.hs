0c24e58885f1f1f5664b0706510cf7cbe77cbfcc
pocari <caffelattenonsugar@gmail.com>
2019-12-12 21:45:10 +0900

    8.9.1 mult

--- 8/chap8.hs
+++ 8/chap8.hs
@@ -50,6 +50,10 @@ add :: Nat -> Nat -> Nat
 add Zero     n = n
 add (Succ m) n = Succ (add m n)
 
+mult :: Nat -> Nat -> Nat
+mult Zero     _ = Zero
+mult (Succ m) n = add n (mult m n)
+
 safediv :: Int -> Int -> Maybe Int
 safediv _ 0 = Nothing
 safediv x y = Just (x `div` y)
