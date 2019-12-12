0922108e34efd217a65bba294acfae98cba0cac3
pocari <caffelattenonsugar@gmail.com>
2019-12-13 00:06:38 +0900

    8.9.9 add mult

--- 8/abstract_machine.hs
+++ 8/abstract_machine.hs
@@ -1,22 +1,22 @@
 module Chap8AbstraceMachine where
 
-data Expr = Val Int | Add Expr Expr
+data Expr = Val Int | Add Expr Expr | Mult Expr Expr
           deriving Show
 
-data Op = EVAL Expr | ADD Int
+data Op = ADD Expr | MULT Expr
         deriving Show
 
 type Cont = [Op]
 
 eval :: Expr -> Cont -> Int
-eval (Val n  ) c = exec c n
-eval (Add x y) c = eval x (EVAL y : c)
-
+eval (Val n   ) c = exec c n
+eval (Add  x y) c = eval x (ADD y : c)
+eval (Mult x y) c = eval x (MULT y : c)
 
 exec :: Cont -> Int -> Int
 exec []           n = n
-exec (EVAL y : c) n = eval y (ADD n : c)
-exec (ADD  n : c) m = exec c (n + m)
+exec (ADD  e : c) m = exec c (eval e c) + m
+exec (MULT e : c) m = exec c (eval e c) * m
 
 -- eval (Add (Val 1) (Val 2)) []
 -- eval (Val 1) [(EVAL (Val 2))]
