120a29177f0cce36790a859fdfe5352ad9313fa3
pocari <caffelattenonsugar@gmail.com>
2019-12-12 22:57:36 +0900

    8.9.3 balanced

---
+++ 8/8.9.3.hs
@@ -0,0 +1,50 @@
+module Chap893 where
+
+data Tree a = Leaf a | Node (Tree a) (Tree a)
+
+--             .
+--          /     \
+--        .         .
+--       / \       / \
+--      1   2     3   4
+--
+t1 :: Tree Int
+t1 = Node (Node (Leaf 1) (Leaf 2)) (Node (Leaf 3) (Leaf 4))
+
+--             .
+--          /     \
+--        .         3
+--       / \
+--      1   2
+--
+t2 :: Tree Int
+t2 = Node (Node (Leaf 1) (Leaf 2)) (Leaf 3)
+
+--             .
+--           /   \
+--          1     .
+--               / \
+--              2   3
+--
+t3 :: Tree Int
+t3 = Node (Leaf 1) (Node (Leaf 2) (Leaf 3))
+
+--             .
+--           /   \
+--          1     .
+--               / \
+--              .   4
+--             / \
+--            2   3
+--
+t4 :: Tree Int
+t4 = Node (Leaf 1) (Node (Node (Leaf 2) (Leaf 3)) (Leaf 4))
+
+countLeaf :: Tree a -> Int
+countLeaf (Leaf _  ) = 1
+countLeaf (Node l r) = countLeaf l + countLeaf r
+
+balanced :: Tree a -> Bool
+balanced (Leaf _  ) = True
+balanced (Node x y) = abs (countLeaf x - countLeaf y) <= 1
+
