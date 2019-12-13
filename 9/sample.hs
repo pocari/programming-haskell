62d358e45a0f11f70349caf4b8c0f7d54dab87ac
pocari <caffelattenonsugar@gmail.com>
2019-12-13 09:01:20 +0900

    〜9.4

---
+++ 9/sample.hs
@@ -0,0 +1,51 @@
+module Sample where
+
+data Op = Add | Sub | Mul | Div
+
+instance Show Op where
+  show Add = "+"
+  show Sub = "-"
+  show Mul = "*"
+  show Div = "/"
+
+valid :: Op -> Int -> Int -> Bool
+valid Add _ _ = True
+valid Sub x y = x > y
+valid Mul _ _ = True
+valid Div x y = x `mod` y == 0
+
+apply :: Op -> Int -> Int -> Int
+apply Add x y = x + y
+apply Sub x y = x - y
+apply Mul x y = x * y
+apply Div x y = x `div` y
+
+data Expr = Val Int | App Op Expr Expr
+
+instance Show Expr where
+  show (Val n    ) = show n
+  show (App o l r) = brak l ++ show o ++ brak r
+   where
+    brak (Val e) = show e
+    brak e       = "(" ++ show e ++ ")"
+
+values :: Expr -> [Int]
+values (Val e    ) = [e]
+values (App _ l r) = values l ++ values r
+
+eval :: Expr -> [Int]
+eval (Val e    ) = [ e | e > 0 ]
+eval (App o l r) = [ apply o x y | x <- eval l, y <- eval r, valid o x y ]
+
+subs :: [a] -> [[a]]
+subs []       = [[]]
+subs (x : xs) = yss ++ map (x :) yss where yss = subs xs
+
+interleave :: a -> [a] -> [[a]]
+interleave x []       = [[x]]
+interleave x (y : ys) = (x : y : ys) : map (y :) (interleave x ys)
+
+perms :: [a] -> [[a]]
+perms []       = [[]]
+perms (x : xs) = concatMap (interleave x) (perms xs)
+
