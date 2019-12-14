c621e781c3bf33d0c931157db7722bf452ea8a1e
pocari <caffelattenonsugar@gmail.com>
2019-12-14 16:57:01 +0900

    〜10.6 hangman

--- 10/sample.hs
+++ 10/sample.hs
@@ -1,5 +1,7 @@
 module Sample where
 
+import           System.IO
+
 wrapper :: IO ()
 wrapper = do
   (x, y) <- act
@@ -26,7 +28,6 @@ getLine' = do
       xs <- getLine'
       return (x : xs)
 
-
 putStr' :: String -> IO ()
 putStr' []       = return ()
 putStr' (x : xs) = do
@@ -49,3 +50,41 @@ strlen = do
   putStr' (show (length x))
   putStrLn' " characters."
 
+sgetLine :: IO String
+sgetLine = do
+  x <- getCh
+  if x == '\n'
+    then do
+      putChar x
+      return []
+    else do
+      putChar '-'
+      xs <- sgetLine
+      return (x : xs)
+
+getCh :: IO Char
+getCh = do
+  hSetEcho stdin False
+  x <- getChar
+  hSetEcho stdin True
+  return x
+
+match :: String -> String -> String
+match xs ys = [ if x `elem` ys then x else '-' | x <- xs ]
+
+play :: String -> IO ()
+play word = do
+  putStr "? "
+  guess <- getLine
+  if guess == word
+    then putStrLn "You got it!!"
+    else do
+      putStrLn (match word guess)
+      play word
+
+hangman :: IO ()
+hangman = do
+  putStrLn "Think of a word:"
+  word <- sgetLine
+  putStrLn "Try to guess it:"
+  play word
