13574bde86bed07412f876e0b3b7e38d1e3124d8
pocari <caffelattenonsugar@gmail.com>
2019-12-14 13:55:08 +0900

    9.11.6.a

--- 9/sample.hs
+++ 9/sample.hs
@@ -1,42 +1,45 @@
 module Sample where
 
-data Op = Add | Sub | Mul | Div
+data Op = Add | Sub | Mul | Div | Pow
 
 instance Show Op where
   show Add = "+"
   show Sub = "-"
   show Mul = "*"
   show Div = "/"
+  show Pow = "^"
 
--- valid :: Op -> Int -> Int -> Bool
+-- valid :: Op -> Integer -> Integer -> Bool
 -- valid Add _ _ = True
 -- valid Sub x y = x > y
 -- valid Mul _ _ = True
 -- valid Div x y = x `mod` y == 0
 
--- valid :: Op -> Int -> Int -> Bool
--- valid Add x y = x <= y
--- valid Sub x y = x > y
--- valid Mul x y = x /= 1 && y /= 1 && x <= y
--- valid Div x y = x `mod` y == 0
+valid :: Op -> Integer -> Integer -> Bool
+valid Add x y = x <= y
+valid Sub x y = x > y
+valid Mul x y = x /= 1 && y /= 1 && x <= y
+valid Div x y = y /= 0 && x `mod` y == 0
+valid Pow _ _ = True
 
 -- 9.11.5 のvalid
 -- length [e | ns' <- choices [1, 3, 7, 10, 25, 50], e <- exprs ns', not $ null $ eval e]
 -- 10839369
 -- になる
-valid :: Op -> Int -> Int -> Bool
-valid Add _ _ = True
-valid Sub x y = True
-valid Mul _ _ = True
-valid Div x y = y /= 0 && x `mod` y == 0
+-- valid :: Op -> Integer -> Integer -> Bool
+-- valid Add _ _ = True
+-- valid Sub x y = True
+-- valid Mul _ _ = True
+-- valid Div x y = y /= 0 && x `mod` y == 0
 
-apply :: Op -> Int -> Int -> Int
+apply :: Op -> Integer -> Integer -> Integer
 apply Add x y = x + y
 apply Sub x y = x - y
 apply Mul x y = x * y
 apply Div x y = x `div` y
+apply Pow x y = x ^ y
 
-data Expr = Val Int | App Op Expr Expr
+data Expr = Val Integer | App Op Expr Expr
 
 instance Show Expr where
   show (Val n    ) = show n
@@ -48,11 +51,11 @@ instance Show Expr where
 e1 :: Expr
 e1 = App Mul (App Add (Val 1) (Val 50)) (App Sub (Val 25) (Val 10))
 
-values :: Expr -> [Int]
+values :: Expr -> [Integer]
 values (Val e    ) = [e]
 values (App _ l r) = values l ++ values r
 
-eval :: Expr -> [Int]
+eval :: Expr -> [Integer]
 eval (Val e    ) = [ e | e > 0 ]
 eval (App o l r) = [ apply o x y | x <- eval l, y <- eval r, valid o x y ]
 
@@ -75,7 +78,7 @@ choices = concatMap perms . subs
 choices' :: [a] -> [[a]]
 choices' ns = [ ps | sub <- subs ns, ps <- perms sub ]
 
-solution :: Expr -> [Int] -> Int -> Bool
+solution :: Expr -> [Integer] -> Integer -> Bool
 solution e xs n = elem (values e) (choices xs) && eval e == [n]
 
 split :: [a] -> [([a], [a])]
@@ -92,15 +95,15 @@ combine :: Expr -> Expr -> [Expr]
 combine l r = [ App o l r | o <- ops ]
 
 ops :: [Op]
-ops = [Add, Sub, Mul, Div]
+ops = [Add, Sub, Mul, Div, Pow]
 
-exprs :: [Int] -> [Expr]
+exprs :: [Integer] -> [Expr]
 exprs []  = []
 exprs [n] = [Val n]
 exprs ns =
   [ e | (ls, rs) <- split ns, l <- exprs ls, r <- exprs rs, e <- combine l r ]
 
-solutions :: [Int] -> Int -> [Expr]
+solutions :: [Integer] -> Integer -> [Expr]
 solutions ns n = [ e | ns' <- choices ns, e <- exprs ns', eval e == [n] ]
 
 
@@ -114,9 +117,9 @@ solutions ns n = [ e | ns' <- choices ns, e <- exprs ns', eval e == [n] ]
 -- でいけるとおもったけど、366922 になってしまった。
 -- とおもったけど、章中ででてきた改良validが使われてるせいだった、旧validを使うとちゃんと 4672540に。
 
-type Result = (Expr, Int)
+type Result = (Expr, Integer)
 
-results :: [Int] -> [Result]
+results :: [Integer] -> [Result]
 results []  = []
 results [n] = [ (Val n, n) | n > 0 ]
 results ns =
@@ -130,7 +133,7 @@ results ns =
 combine' :: Result -> Result -> [Result]
 combine' (l, x) (r, y) = [ (App o l r, apply o x y) | o <- ops, valid o x y ]
 
-solutions' :: [Int] -> Int -> [Expr]
+solutions' :: [Integer] -> Integer -> [Expr]
 solutions' ns n = [ e | ns' <- choices ns, (e, m) <- results ns', m == n ]
 
 -- 9.11.2
