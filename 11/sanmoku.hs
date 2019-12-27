ca0837dd0ee557f46f6f8cbcabd6689754909f01
pocari <caffelattenonsugar@gmail.com>
2019-12-27 17:33:40 +0900

    関数名間違ってたので修正

--- 11/sanmoku.hs
+++ 11/sanmoku.hs
@@ -181,8 +181,8 @@ minimax :: Tree Grid -> Tree (Grid, Player)
 minimax (Node g []) | wins O g  = Node (g, O) []
                     | wins X g  = Node (g, X) []
                     | otherwise = Node (g, B) []
-minmax (Node g ts) | turn g == O = Node (g, minimum ps) ts'
-                   | turn g == X = Node (g, maximum ps) ts'
+minimax (Node g ts) | turn g == O = Node (g, minimum ps) ts'
+                    | turn g == X = Node (g, maximum ps) ts'
  where
   ts' = map minimax ts
   ps  = [ p | Node (_, p) _ <- ts' ]
@@ -191,7 +191,7 @@ bestmove :: Grid -> Player -> Grid
 bestmove g p = head [ g' | Node (g', p') _ <- ts, p' == best ]
  where
   tree              = prune depth (gametree g p)
-  Node (_, best) ts = minmax tree
+  Node (_, best) ts = minimax tree
 
 main :: IO ()
 main = do
