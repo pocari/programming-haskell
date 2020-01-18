5dab5ee00439a469716e837379e3e49af874e835
pocari <caffelattenonsugar@gmail.com>
2020-01-18 15:51:05 +0900

    〜16.8

--- 16/sample.hs
+++ 16/sample.hs
@@ -28,3 +28,50 @@ flatten' :: Tree a -> [a] -> [a]
 flatten' (Leaf x  ) ys = x : ys
 flatten' (Node l r) ys = flatten' l (flatten' r ys)
 
+data Expr = Val Int | Add Expr Expr
+          deriving Show
+
+e1 :: Expr
+e1 = Add (Add (Add (Val 1) (Val 2)) (Val 3)) (Val 4)
+
+eval :: Expr -> Int
+eval (Val n  ) = n
+eval (Add l r) = eval l + eval r
+
+-- *Sample> e1
+-- Add (Add (Add (Val 1) (Val 2)) (Val 3)) (Val 4)
+-- *Sample> eval e1
+-- 10
+
+type Stack = [Int]
+
+type Code = [Op]
+
+data Op = PUSH Int | ADD
+        deriving Show
+
+exec :: Code -> Stack -> Stack
+exec []           s           = s
+exec (PUSH n : c) s           = exec c (n : s)
+exec (ADD    : c) (x : y : s) = exec c ((x + y) : s)
+exec _            _           = error "invalid pattern"
+
+comp1 :: Expr -> Code
+comp1 (Val n  ) = [PUSH n]
+comp1 (Add l r) = comp1 l ++ comp1 r ++ [ADD]
+
+comp2 :: Expr -> Code
+comp2 e = comp' e []
+
+comp' :: Expr -> Code -> Code
+comp' (Val n  ) c = PUSH n : c
+comp' (Add l r) c = comp' l (comp' r (ADD : c))
+
+-- *Sample> e1
+-- Add (Add (Add (Val 1) (Val 2)) (Val 3)) (Val 4)
+-- *Sample> comp e1
+-- [PUSH 1,PUSH 2,ADD,PUSH 3,ADD,PUSH 4,ADD]
+-- *Sample> exec (comp e1) []
+-- [10]
+
+
