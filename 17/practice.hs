4ef7c149cfe890ec82fc8ec8b9f9377d7c2faead
pocari <caffelattenonsugar@gmail.com>
2020-01-21 08:46:51 +0900

    17.8.1 まず普通のeval定義

--- 17/practice.hs
+++ 17/practice.hs
@@ -1,10 +1,30 @@
 module Sample where
 
-data Expr = Val Int | Add Expr Expr
+data Expr = Val Int
+          | Add Expr Expr
+          | Throw
+          | Catch Expr Expr
           deriving Show
 
 e1 :: Expr
 e1 = Add (Add (Val 1) (Val 2)) (Val 3)
+
+e2 :: Expr
+e2 = Add (Add (Add (Val 1) (Val 2)) (Val 3)) Throw
+
+e3 :: Expr
+e3 = Catch (Add (Val 1) (Val 2)) (Val 99)
+
+e4 :: Expr
+e4 = Catch (Add Throw (Val 2)) (Val 99)
+
+eval :: Expr -> Maybe Int
+eval (Val n  )   = Just n
+eval (Add x y)   = (+) <$> eval x <*> eval y
+eval Throw       = Nothing
+eval (Catch x h) = case eval x of
+  Just n  -> Just n
+  Nothing -> eval h
 
 type Stack = [Int]
 
@@ -14,9 +34,6 @@ push n s = n : s
 add :: Stack -> Stack
 add (m : n : xs) = (n + m) : xs
 add _            = error "invalid add operation"
-
-eval :: Expr -> Int
-eval e = head $ exec (comp e) []
 
 type Cont = Stack -> Stack
 
